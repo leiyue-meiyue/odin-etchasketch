cfddbf12a644d8d05d13f8e6a2275580e6bccf62
leiyue <157659887+leiyue-meiyue@users.noreply.github.com>
2024-06-07 19:25:50 -0400

    Complete change grid size functionality

--- script.js
+++ script.js
@@ -6,28 +6,53 @@
 
 const boxesContainer = document.querySelector(".boxes-container");
 
+// initialize
 let numInRow = 16;
+let containerWidth = 600;
+let gap = 1;
+let border = 0;
 
+function calculateSquareWidth (containerWidth, numInRow, gap, border) {
+  let squareWidth = (containerWidth + gap) / numInRow - (2 * border) - gap;
 
-function createPad (numInRow) {
+  // squareWidth = (600 + 2) / 25 - (2 * 2) - 2
+  //             = 18.08
+
+  // squareWidth = (600 + 2) / 30 - (2 * 2) - 2
+  //             = 
+  // squareWidth = Math.round(squareWidth * 1000) / 1000
+
+  return squareWidth
+}
+
+let squareWidth = calculateSquareWidth(containerWidth, numInRow, gap, border);
+
+function createPad (numInRow, squareWidth) {
   let gridSize = numInRow * numInRow;
   for (i = 0; i < gridSize; i++) {
-    // create element and add as child to container
     const square = document.createElement("div");
     square.classList.add("square");
+    square.style.setProperty("width", `${squareWidth}px`);
+    square.style.setProperty("height", `${squareWidth}px`);
+    // square.style.setProperty("width", `100%`);
+
     boxesContainer.appendChild(square);
-  
+    console.log(square);
+
     square.addEventListener("mouseenter", () => {
       square.classList.add("black-bg");
     })
   }
 }
 
-createPad(numInRow);
+createPad(numInRow, squareWidth);
 
 const changeGridSize = document.querySelector("#change-grid-size");
 changeGridSize.addEventListener("click", () => {
-  numInRow = prompt("Enter a number between 1 and 100");
+  numInRow = prompt("Enter the number of squares on each row (max 100)");
   boxesContainer.replaceChildren();       // remove boxes
-  createPad(numInRow);
+  squareWidth = calculateSquareWidth(containerWidth, numInRow, gap, border);
+  console.log(squareWidth);
+  createPad(numInRow, squareWidth);
 })
+
